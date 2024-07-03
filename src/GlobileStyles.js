7ee70c05ab2ea22c97153bee938af60ca846b1a4
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-03 08:52:57 +0200

    Created GlobalStyles, moved styles from index css file here

---
+++ src/GlobileStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    };
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    };
+
+    main {
+        font-family: "Lexend Deca", sans-serif;
+        background-color: #eee;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 100vh;
+        padding: 10px;
+    };
+`;
