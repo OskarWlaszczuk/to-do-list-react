1aee788e61b3a4d0676dc59d2558af5b8b1a3267
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-04 14:55:27 +0200

    ButtonsBar, and ButtonsBarItem styled-components imported, and pasted List component

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,22 +1,27 @@
-import { List, Item, Button, ToggleDoneButton, Content } from "./styled";
+import { List, Item, Button, ToggleDoneButton, Content, ButtonsBar, ButtonsBarItem} from "./styled";
 
 const Tasks = ({ tasks, hideDoneTasks, removeTasks, toggleTasksDone }) => {
-    return (
-        <List>
-            {
-                tasks.map(({ id, done, content }) => {
-                    return (!hideDoneTasks || (hideDoneTasks && !done)) &&
-                        (
-                            <Item key={id}>
-                                <ToggleDoneButton onClick={() => toggleTasksDone(id)}>{done ? "✔" : ""}</ToggleDoneButton>
-                                <Content $donedItem={done}>{content}</Content>
-                                <Button onClick={() => removeTasks(id)}>🗑️</Button>
-                            </Item>
-                        );
-                })
-            }
-        </List>
-    )
-}
+  return (
+    <List>
+      {tasks.map(({ id, done, content }) => {
+        return (
+          (!hideDoneTasks || (hideDoneTasks && !done)) && (
+            <Item key={id}>
+              <ButtonsBar>
+                <ButtonsBarItem>B</ButtonsBarItem>
+                <ButtonsBarItem $italic>I</ButtonsBarItem>
+              </ButtonsBar>
+              <ToggleDoneButton onClick={() => toggleTasksDone(id)}>
+                {done ? "✔" : ""}
+              </ToggleDoneButton>
+              <Content $donedItem={done}>{content}</Content>
+              <Button onClick={() => removeTasks(id)}>🗑️</Button>
+            </Item>
+          )
+        );
+      })}
+    </List>
+  );
+};
 
 export default Tasks;
