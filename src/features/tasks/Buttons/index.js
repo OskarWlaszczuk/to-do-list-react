bc76497463956ae03496829b4c74528aada3b312
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-16 21:20:00 +0200

    Tasks, and hideDoneTasks states imported, with action-creators from react

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,16 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { toggleHideDoneTasks, toggleAllTaskDone } from "../tasksSlice";
+import { selectTasks } from "../tasksSlice";
 import { Section, Button } from "./styled";
 
-const Buttons = ({ tasks, hideDoneTasks, toggleHideDoneTasks, toggleAllTaskDone }) => {
+const Buttons = () => {
+    const { tasks, hideDoneTasks } = useSelector(selectTasks)
+    const dispatch = useDispatch();
 
     return (
         tasks.length > 0 && (
             <Section>
-                <Button onClick={toggleHideDoneTasks}>
-                    {hideDoneTasks && tasks.some(({ done }) => done) ? 'Pokaż' : 'Ukryj'} ukończone
+                <Button onClick={() => dispatch(toggleHideDoneTasks())}>
+                    {
+                        hideDoneTasks && tasks.some(({ done }) => done) ?
+                            'Pokaż' :
+                            'Ukryj'
+                    } ukończone
                 </Button>
                 <Button
                     disabled={tasks.every(({ done }) => done)}
-                    onClick={toggleAllTaskDone}
+                    onClick={() => dispatch(toggleAllTaskDone())}
                 >
                     Ukończ wszystkie
                 </Button>
