cde62875bbcefbdcdd0df3d3d2fc6ff11637534d
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-16 21:16:13 +0200

    Removed unnecessary states, functions and props, now components gets states, and actions directly from redux

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -3,54 +3,21 @@ import MainHeader from "../../common/MainHeader";
 import Form from "./Form";
 import Section from "../../common/Section";
 import HeaderContent from "../../common/HeaderContent";
-import Buttons from "./Buttons";
 import Stats from "./Stats";
-import TasksList from "./TasksList";
-import { useTasks } from "./useTasks";
-import { useHideDoneTasks } from "./useHideDoneTasks";
+import TasksList from "./TasksList"
 
 export function Tasks() {
-  const {
-    tasks,
-    removeTasks,
-    toggleTasksDone,
-    toggleAllTaskDone,
-    addNewTaskContent,
-    toggleImportantContent,
-  } = useTasks();
-
-  const { hideDoneTasks, toggleHideDoneTasks } = useHideDoneTasks(tasks);
-
   return (
     <>
       <MainHeader />
-      <Form addNewTaskContent={addNewTaskContent} />
-      <Section
-        children={
-          <React.Fragment>
-            <HeaderContent
-              title="Lista zadań"
-              children={
-                //Buttons przenieść, do środka HeadeContent, nie jako chldren
-                <Buttons
-                  tasks={tasks}
-                  hideDoneTasks={hideDoneTasks}
-                  toggleHideDoneTasks={toggleHideDoneTasks}
-                  toggleAllTaskDone={toggleAllTaskDone}
-                />
-              }
-            />
-            <Stats tasks={tasks} />
-            <TasksList
-              tasks={tasks}
-              hideDoneTasks={hideDoneTasks}
-              removeTasks={removeTasks}
-              toggleTasksDone={toggleTasksDone}
-              toggleImportantContent={toggleImportantContent}
-            />
-          </React.Fragment>
-        }
-      />
+      <Form />
+      <Section body={
+        <>
+          <HeaderContent title="Lista zadań" />
+          <Stats />
+          <TasksList />
+        </>
+      } />
     </>
   );
 };
