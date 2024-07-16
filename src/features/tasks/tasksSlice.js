619342a8b690351c05ba08643ebc32c6e77fa3ed
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-16 21:18:34 +0200

    Slice of tasks created, action-creators added, initial states of tasks, and hideDoneTasks variables declared, action-creators, reducer, and states exported

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -1,16 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 export const tasksSlice = createSlice({
     name: 'tasks',
     initialState: {
         tasks: [],
+        hideDoneTasks: false,
     },
     reducers: {
         addTask: ({ tasks }, { payload }) => {
             tasks.push(payload);
         },
+        toggleTaskDone: ({ tasks }, { payload }) => {
+            const index = tasks.findIndex(({ id }) => id === payload);
+            tasks[index].done = !tasks[index].done;
+        },
+        removeTask: ({ tasks }, { payload }) => {
+            const index = tasks.findIndex(({ id }) => id === payload);
+            tasks.splice(index, 1);
+        },
+        toggleImportantContent: ({ tasks }, { payload }) => {
+            const index = tasks.findIndex(({ id }) => id === payload);
+            tasks[index].important = !tasks[index].important;
+        },
+        toggleHideDoneTasks: (state) => {
+            if (state.tasks.some(({ done }) => done)) {
+                state.hideDoneTasks = !state.hideDoneTasks
+            };
+        },
+        toggleAllTaskDone: ({ tasks }) => {
+            tasks.forEach(task => {
+                if (!task.done) {
+                    task.done = !task.done;
+                }
+            });
+        }
     },
 });
 
-export const { addTask } = tasksSlice.actions;
-export const selectTasks = state => state.tasks;
+export const { addTask, toggleTaskDone, removeTask, toggleHideDoneTasks, toggleImportantContent, toggleAllTaskDone } = tasksSlice.actions;
+export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
