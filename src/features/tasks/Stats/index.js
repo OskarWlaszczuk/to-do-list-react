3785479f4db341293df30cbbe53003ac56d9d1fd
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-16 21:22:47 +0200

    Replaced tasks as props, with tasks state imported form tasksSlice

--- src/features/tasks/Stats/index.js
+++ src/features/tasks/Stats/index.js
@@ -1,14 +1,17 @@
+import { useSelector } from "react-redux";
 import { Content, Item } from "./styled";
+import { selectTasks } from "../tasksSlice";
 
-const Stats = ({ tasks }) => (
-    tasks.length > 0 && (
+const Stats = () => {
+    const { tasks } = useSelector(selectTasks)
+
+    return (tasks.length > 0 && (
         <Content>
             <Item>Liczba wszystkich zadań: {tasks.length}</Item>
             <Item $sumOfFinishedTasks>Liczba ukończonych zadań: {tasks.filter(({ done }) => done).length}</Item>
             <Item $sumOfImportantTasks>Liczba ważnych zadań: {tasks.filter(({ important }) => important).length}</Item>
         </Content>
-
-    )
-);
+    ));
+};
 
 export default Stats;
