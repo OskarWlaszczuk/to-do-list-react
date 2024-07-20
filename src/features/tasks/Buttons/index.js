604a389aa6eb7c26087575114eee45c4b154fbfe
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-20 11:29:03 +0200

    'Pobierz przykładowe zadania' button added, which dispatch downloadExampleTasks action on click

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,31 +1,43 @@
 import { useDispatch, useSelector } from "react-redux";
-import { toggleHideDoneTasks, toggleAllTaskDone } from "../tasksSlice";
-import { selectTasksState, selectIsAllDone } from "../tasksSlice";
+import { toggleHideDoneTasks, toggleAllTaskDone, downloadExampleTasks } from "../tasksSlice";
+import { selectTasks, selectHideDoneTasks, selectIsAllTasksDone, selectIsTasksEmpty } from "../tasksSlice";
 import { Section, Button } from "./styled";
 
 const Buttons = () => {
-    const { tasks, hideDoneTasks } = useSelector(selectTasksState)
-    const { isAllDone } = useSelector(selectIsAllDone);
+    const tasks = useSelector(selectTasks)
+    const hideDoneTasks = useSelector(selectHideDoneTasks);
+    const isAllDone = useSelector(selectIsAllTasksDone);
+    const isTasksEmpty = useSelector(selectIsTasksEmpty);
+
     const dispatch = useDispatch();
-    console.log(isAllDone)
+
     return (
-        tasks.length > 0 && (
-            <Section>
-                <Button onClick={() => dispatch(toggleHideDoneTasks())}>
-                    {
-                        hideDoneTasks && tasks.some(({ done }) => done) ?
-                            'Pokaż' :
-                            'Ukryj'
-                    } ukończone
-                </Button>
-                <Button
-                    disabled={isAllDone}
-                    onClick={() => dispatch(toggleAllTaskDone())}
-                >
-                    Ukończ wszystkie
-                </Button>
-            </Section>
-        ))
+
+        <Section>
+            <Button
+                onClick={() => dispatch(downloadExampleTasks())}
+            >
+                Pobierz przykładowe zadania
+            </Button>
+            {!isTasksEmpty && (
+                <>
+                    <Button onClick={() => dispatch(toggleHideDoneTasks())}>
+                        {
+                            hideDoneTasks && tasks.some(({ done }) => done) ?
+                                'Pokaż' :
+                                'Ukryj'
+                        } ukończone
+                    </Button>
+                    <Button
+                        disabled={isAllDone}
+                        onClick={() => dispatch(toggleAllTaskDone())}
+                    >
+                        Ukończ wszystkie
+                    </Button>
+                </>
+            )}
+        </Section>
+    )
 };
 
 export default Buttons;
