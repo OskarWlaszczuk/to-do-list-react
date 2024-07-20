58a0aa8d197fc7adc6b0046bfd99d27b94493303
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-20 11:27:14 +0200

    setting locale storage as initial state in tasks

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -1,26 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getInitialTasks } from './tasksLocaleStorage';
+import { tasksListKey } from '../../tasksListKey';
 
 export const tasksSlice = createSlice({
     name: 'tasks',
     initialState: {
-        tasks: [],
+        tasks: getInitialTasks(tasksListKey),
         hideDoneTasks: false,
-        isAllDone: false
     },
     reducers: {
-        addTask: ({ tasks }, { payload }) => {
-            tasks.push(payload);
+        addTask: ({ tasks }, { payload: task }) => {
+            tasks.push(task);
         },
-        toggleTaskDone: ({ tasks }, { payload }) => {
-            const index = tasks.findIndex(({ id }) => id === payload);
+        toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
             tasks[index].done = !tasks[index].done;
         },
-        removeTask: ({ tasks }, { payload }) => {
-            const index = tasks.findIndex(({ id }) => id === payload);
+        removeTask: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
             tasks.splice(index, 1);
         },
-        toggleImportantContent: ({ tasks }, { payload }) => {
-            const index = tasks.findIndex(({ id }) => id === payload);
+        toggleImportantContent: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
             tasks[index].important = !tasks[index].important;
         },
         toggleHideDoneTasks: (state) => {
@@ -28,21 +29,36 @@ export const tasksSlice = createSlice({
                 state.hideDoneTasks = !state.hideDoneTasks
             };
         },
-        toggleAllTaskDone: ({ tasks, isAllDone }) => {
-            if (tasks.every(({ done }) => done)) {
-                isAllDone = !isAllDone;
-            }
-            console.log(isAllDone);
+        toggleAllTaskDone: ({ tasks }) => {
             tasks.forEach(task => {
                 if (!task.done) {
                     task.done = !task.done;
                 }
             });
-        }
+        },
+        downloadExampleTasks: () => { },
+        setTasks: (state, { payload: tasks }) => {
+            state.tasks = tasks;
+        },
     },
 });
 
-export const { addTask, toggleTaskDone, removeTask, toggleHideDoneTasks, toggleImportantContent, toggleAllTaskDone } = tasksSlice.actions;
-export const selectTasksState = (state) => state.tasks;
-export const selectIsAllDone = state => state.tasks
-export default tasksSlice.reducer;
+export const {
+    addTask,
+    toggleTaskDone,
+    removeTask,
+    toggleHideDoneTasks,
+    toggleImportantContent,
+    toggleAllTaskDone,
+    downloadExampleTasks,
+    setTasks,
+} = tasksSlice.actions;
+
+const selectTasksState = state => state.tasks;
+
+export const selectTasks = state => selectTasksState(state).tasks;
+export const selectHideDoneTasks = state => selectTasksState(state).hideDoneTasks;
+export const selectIsAllTasksDone = state => selectTasks(state).every(({ done }) => done);
+export const selectIsTasksEmpty = state => selectTasks(state).length === 0;
+
+export const tasksReducer = tasksSlice.reducer;
