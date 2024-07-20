10eb85516350dcbcc4b654dc60ba5a410b083d67
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-20 11:34:43 +0200

    Added title as prop, removed unnecessary style attribudes from Items, displaying sum of doned tasks in percents

--- src/features/tasks/Stats/index.js
+++ src/features/tasks/Stats/index.js
@@ -1,15 +1,26 @@
 import { useSelector } from "react-redux";
-import { Content, Item } from "./styled";
-import { selectTasksState } from "../tasksSlice";
+import { Content, ItemWrapper, Item } from "./styled";
+import { Header } from "../../../common/HeaderContent/styled"
+import { selectTasks, selectIsTasksEmpty } from "../tasksSlice";
 
-const Stats = () => {
-    const { tasks } = useSelector(selectTasksState)
+const Stats = ({ title }) => {
+    const tasks = useSelector(selectTasks)
+    const isTasksEmpty = useSelector(selectIsTasksEmpty);
 
-    return (tasks.length > 0 && (
+    return (!isTasksEmpty && (
         <Content>
-            <Item>Liczba wszystkich zadań: {tasks.length}</Item>
-            <Item $sumOfFinishedTasks>Liczba ukończonych zadań: {tasks.filter(({ done }) => done).length}</Item>
-            <Item $sumOfImportantTasks>Liczba ważnych zadań: {tasks.filter(({ important }) => important).length}</Item>
+            <Header $stats>{title}</Header>
+            <ItemWrapper>
+                <Item>Liczba wszystkich zadań: <strong>{tasks.length}</strong></Item>
+                <Item >
+                    Liczba ukończonych zadań: <strong>{((tasks.filter(({ done }) => done).length / tasks.length) * 100).toFixed(0)}%</strong>
+                </Item>
+                <Item>
+                    Liczba ważnych zadań: <strong>
+                        {tasks.filter(({ important, done }) => important && !done).length}
+                    </strong>
+                </Item>
+            </ItemWrapper>
         </Content>
     ));
 };
