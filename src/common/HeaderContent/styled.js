557e5ce6949890b3ca54487cc26064b40c82853e
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-20 11:13:04 +0200

    Container items aligned to their baseline, height of grid-rows changed to auto

--- src/common/HeaderContent/styled.js
+++ src/common/HeaderContent/styled.js
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
     display: grid;
     grid-gap: 10px;
-    align-items: center;
+    align-items: baseline;
     padding: 15px 0px;
     grid-template-columns: repeat(2, 1fr);
     border-bottom: 2px solid rgba(179, 174, 174, 0.742);
@@ -12,7 +12,7 @@ export const Container = styled.div`
 
     @media (max-width:${({ theme }) => theme.breakPoints.smallLaptop}px) {
         grid-template-columns: 1fr;
-        grid-template-rows: repeat(2, 1fr);
+        grid-template-rows: repeat(2, auto);
     };
 `;
 
@@ -20,8 +20,14 @@ export const Header = styled.header`
     font-weight: bold;
     font-size: 20px;
 
+
     @media (max-width:${({ theme }) => theme.breakPoints.mobileL}px) {
         justify-self: center;
         text-align: center;
     };
+
+    ${({ $stats }) => $stats && css`
+        margin-top: 20px;
+        margin-bottom: 20px;
+    `};
 `;
