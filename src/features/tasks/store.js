d0162ee75c58ddc0eedb4250f8033b1c3a3d6765
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-20 11:22:23 +0200

    Added middleware to store, induced rootSaga

--- src/features/tasks/store.js
+++ src/features/tasks/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from './tasksSlice';
-export default configureStore({
+import { tasksReducer } from './tasksSlice';
+import createSagaMiddleware from "redux-saga"
+import { rootSaga } from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         tasks: tasksReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
