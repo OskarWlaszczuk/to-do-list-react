f853382df67d6554485ca9359561a11fb0b4ff85
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-22 10:31:31 +0200

    AuthorPage component created, component get title prop, and display it

---
+++ src/features/AuthorPage/index.js
@@ -0,0 +1,11 @@
+import { Header } from "../../common/HeaderContent/styled";
+
+export const AuthorPage = ({ title }) => (
+    <>
+        <Header>{title}</Header>
+        <>
+            {/* Dodać Section */}
+            Vivamus velit odio, varius sit amet leo et, ullamcorper ultrices ante. Nunc justo odio, finibus nec interdum in, porta eget tellus. Nullam eget consequat eros. Donec nec erat ac ex venenatis congue in non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu commodo felis. Nunc nec viverra justo, eu porttitor nisl. Donec elementum non nunc sed rhoncus. In eu risus lorem. Morbi et tortor magna. Curabitur tincidunt ultricies efficitur.
+        </>
+    </>
+);
