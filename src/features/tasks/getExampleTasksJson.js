32070fd3b5150386100103e709952de296849600
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 20:58:06 +0200

    getting example tasks from json function declared

---
+++ src/features/tasks/getExampleTasksJson.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export const getExampleTasksJson = async () => {
+    const response =
+        await axios.get("/to-do-list-react/exampleTasks.json");
+
+    if (response.status !== 200) {
+        throw new Error(response.statusText);
+    }
+    return response.data;
+};
