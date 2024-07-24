b06163f70bbf4a0b8789d70f73c969aca0d9eb15
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 21:11:56 +0200

    Imported selectors, which checks tasks length, and is tasks list empty

--- src/features/tasks/TasksPage/Stats/index.js
+++ src/features/tasks/TasksPage/Stats/index.js
@@ -1,19 +1,22 @@
 import { useSelector } from "react-redux";
 import { Content, ItemWrapper, Item } from "./styled";
 import { Header } from "../../../../common/HeaderContent/styled"
-import { selectTasks, selectIsTasksEmpty } from "../../tasksSlice";
+import { selectTasks, selectIsTasksEmpty, selectTasksLength } from "../../tasksSlice";
 
-const Stats = ({ title }) => {
+const Stats = () => {
     const tasks = useSelector(selectTasks)
+    const tasksLenght = useSelector(selectTasksLength)
     const isTasksEmpty = useSelector(selectIsTasksEmpty);
 
     return (!isTasksEmpty && (
         <Content>
-            <Header $stats>{title}</Header>
+            <Header $stats>Dane zadań 📊</Header>
             <ItemWrapper>
-                <Item>Liczba wszystkich zadań: <strong>{tasks.length}</strong></Item>
+                <Item>Liczba wszystkich zadań: <strong>{tasksLenght}</strong></Item>
                 <Item >
-                    Liczba ukończonych zadań: <strong>{((tasks.filter(({ done }) => done).length / tasks.length) * 100).toFixed(0)}%</strong>
+                    Liczba ukończonych zadań: <strong>{
+                        ((tasks.filter(({ done }) => done).length / tasksLenght) * 100).toFixed(0)}%
+                    </strong>
                 </Item>
                 <Item>
                     Liczba ważnych zadań: <strong>
