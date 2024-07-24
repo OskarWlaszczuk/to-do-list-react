ba13b2a555671de960df1281257e488a2d1c8845
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 20:54:58 +0200

    Renamed styled component to Wrapper, for more clarity

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
-export const Content = styled.section`
+export const Wrapper = styled.section`
     background-color: white;
     border: 2px solid rgba(179, 174, 174, 0.742);
     border-radius: 3px;
     box-shadow: 0px 0px 11px -3px #525251be;
     width: 60%;
     margin-top: 15px;
-    padding: 15px;
+    padding: 20px;
 
     @media (max-width:${({ theme }) => theme.breakPoints.tabletHorizontal}px) {
         width: 100%;
-        padding: 20px;
     };
 `;
