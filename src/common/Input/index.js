9cbcdd2da4320749f58e567473813a96d1196434
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 20:52:15 +0200

    Moved Input styled-component to seperated folder

---
+++ src/common/Input/index.js
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+export const Input = styled.input`
+    padding: 10px;
+    border: 2px solid rgba(179, 174, 174, 0.742);
+    border-radius: 5px;
+
+    ${({ $search }) => $search && css`
+        transition: 1.5s;
+        transform-origin: left;
+        width: 65%;
+
+        @media (max-width:${({ theme }) => theme.breakPoints.mobileL}px) {
+            justify-self: center;
+            width: 100%;
+
+            &:hover {
+                width: 56%;
+                padding-left: 0%;
+            };
+        };
+
+        &:hover {
+            width: 85%;
+            padding-left: 5%;
+        };
+    `};
+`;
