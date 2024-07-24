4ff2f553c0d8c608035b5a2012fa9472102ffd27
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 20:57:39 +0200

    Navigation bar moved to seperated component

--- src/features/tasks/App.js
+++ src/features/tasks/App.js
@@ -1,32 +1,21 @@
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import { TasksPage } from './TasksPage';
-import { AuthorPage } from '../AuthorPage';
-import Section from '../../common/Section';
+import { AuthorPage } from '../author';
 import { TaskContentPage } from './TaskContentPage';
-import { StyledNavLink } from './styled';
+import { NavigationBar } from '../../common/NavigationBar';
 
 export const App = () => (
     <HashRouter>
-        <nav>
-            <ul>
-                <li>
-                    <StyledNavLink activeClassName="yolo" to="/tasks" >Lista zadań</StyledNavLink>
-                </li>
-                <li>
-                    <StyledNavLink activeClassName="yolo" to="/author" >Autor</StyledNavLink>
-                </li>
-            </ul>
-        </nav>
+        <NavigationBar />
         <Switch>
             <Route path="/tasks/:id" >
-                <Section body={<TaskContentPage title="Szczegóły zadania" />} />
+                <TaskContentPage />
             </Route>
             <Route path="/tasks" >
                 <TasksPage />
             </Route>
             <Route path="/author">
-                {/* Dodać Header wewnątrz Section */}
-                <Section body={<AuthorPage title="O autorze" />} />
+                <AuthorPage />
             </Route>
             <Route path="/">
                 <Redirect to="/tasks" />
