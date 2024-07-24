07bf76e590cbe8860119d0675bb7d6b18e304a8f
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 20:56:13 +0200

    AuthorPage component created

---
+++ src/features/author/index.js
@@ -0,0 +1,32 @@
+import Section from "../../common/Section";
+import { Title } from "../../common/Title";
+import { Image } from "./styled";
+import { Content } from "../../common/Content";
+
+export const AuthorPage = () => (
+    <>
+        <Title content="Autor" />
+        <Section
+            title="Oskar Właszczuk"
+            body={
+
+                <>
+                    <Image src="https://avatars.githubusercontent.com/u/155220171?v=4" />
+                    <Content>
+                        Cześć! <br />Jestem młodym, zdeterminowanym i zdescyplinowanym chłopakiem, który z pasją podchodzi do siłowni i programowania. Każdego dnia staram się rozwijać swoje umiejętności, zarówno fizyczne, jak i umysłowe, starając się przy okazji dobrze, przy tym bawić. Moje zaangażowanie w treningi i kodowanie pozwala mi nieustannie pokonywać własne granice i osiągać nowe cele.
+                    </Content>
+                    <Content>
+                        Jeżeli chcesz się dowiedzieć więcej o projekcie, sprawdź moje <a
+                            title="Repozytorium Listy zadań na GitHub"
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://github.com/OskarWlaszczuk/to-do-list-react.git"
+                        >
+                            repozytorium na GitHub
+                        </a>
+                    </Content>
+                </>
+            }
+        />
+    </>
+);
