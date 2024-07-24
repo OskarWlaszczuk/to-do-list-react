5040160a1bc74722479e2a0a9cf9e6c7369f671b
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 21:05:38 +0200

    Replaced HTML elements, with components

--- src/features/tasks/TaskContentPage/index.js
+++ src/features/tasks/TaskContentPage/index.js
@@ -1,28 +1,30 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom/cjs/react-router-dom"
 import { selectTaskById } from "../tasksSlice";
+import Section from "../../../common/Section";
+import { Title } from "../../../common/Title";
+import { NotFound } from "../../../common/NotFound";
 
-
-export const TaskContentPage = ({ title }) => {
+export const TaskContentPage = () => {
     const { id } = useParams();
     const task = useSelector((state) => selectTaskById(state, id));
+
     return (
         <>
-            <h1>{task ? task.content : "Nie znaleziono❌"}</h1>
-            <>
-                <p>
-                    {task ?
-                        <>ID zadania: <strong>{id}</strong></> :
-                        <></>
-                    }
-                </p>
-                <p>
-                    {task ?
-                        <>Ukończone : <strong>{task.done ? "Tak" : "Nie"}</strong></> :
-                        <></>
-                    }
-                </p>
-            </>
+            <Title content="Szczegóły zadania" />
+            <Section
+                title={
+                    task ? task.content : <NotFound content="Nie znaleziono szczegółów zadania" />
+                }
+                body={
+                        <p>
+                            {task ?
+                                <>Ukończone : <strong>{task.done ? "Tak" : "Nie"}</strong></> :
+                                <></>
+                            }
+                        </p>
+                }
+            />
         </>
     );
 };
