23c6268d67f7549abfd995b89153ba6060ecf426
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 21:09:07 +0200

    New selector imported, declared location, with useLocation hook

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -1,16 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
-import {
-    toggleHideDoneTasks,
-    toggleAllTaskDone,
-    downloadExampleTasks
-} from "../../tasksSlice";
-
-import {
-    selectTasks,
-    selectHideDoneTasks,
-    selectIsAllTasksDone,
-    selectIsTasksEmpty
-} from "../../tasksSlice";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { toggleHideDoneTasks, toggleAllTaskDone, selectIsSearchTasksEmpty } from "../../tasksSlice";
+import { selectTasks, selectHideDoneTasks, selectIsAllTasksDone, selectIsTasksEmpty } from "../../tasksSlice";
 import { Section, Button } from "./styled";
 
 const Buttons = () => {
@@ -18,20 +9,19 @@ const Buttons = () => {
     const hideDoneTasks = useSelector(selectHideDoneTasks);
     const isAllDone = useSelector(selectIsAllTasksDone);
     const isTasksEmpty = useSelector(selectIsTasksEmpty);
+    const location = useLocation();
+    const query = (new URLSearchParams(location.search)).get("szukaj");
 
+    const isSearchTasksEmpty = useSelector(state => selectIsSearchTasksEmpty(state, query));
     const dispatch = useDispatch();
 
     return (
-
         <Section>
-            <Button
-                onClick={() => dispatch(downloadExampleTasks())}
-            >
-                Pobierz przykładowe zadania
-            </Button>
             {!isTasksEmpty && (
                 <>
-                    <Button onClick={() => dispatch(toggleHideDoneTasks())}>
+                    <Button
+                        disabled={isSearchTasksEmpty}
+                        onClick={() => dispatch(toggleHideDoneTasks())}>
                         {
                             hideDoneTasks && tasks.some(({ done }) => done) ?
                                 'Pokaż' :
@@ -39,10 +29,10 @@ const Buttons = () => {
                         } ukończone
                     </Button>
                     <Button
-                        disabled={isAllDone}
+                        disabled={isAllDone || isSearchTasksEmpty}
                         onClick={() => dispatch(toggleAllTaskDone())}
                     >
-                        Ukończ wszystkie
+                        {isAllDone ? <>Ukończono</> : <> Ukończ wszystkie</>}
                     </Button>
                 </>
             )}
