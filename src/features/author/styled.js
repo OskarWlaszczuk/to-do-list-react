5ebb5b92866a7aedbec969367f558148f0932619
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-24 20:56:41 +0200

    Image styled-component created

---
+++ src/features/author/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin: 15px auto;
+    display: block;
+`;
