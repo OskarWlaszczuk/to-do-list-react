6503f0b1fe804b25e9ebbf380d63021bc7853649
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-10 18:51:55 +0100

    Converted index.js file to tsx.

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './features/tasks/App';
+import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './GlobileStyles';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import store from './features/tasks/store';
+import { Provider } from 'react-redux';
+//Dodać wszędzie export/import {named}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
