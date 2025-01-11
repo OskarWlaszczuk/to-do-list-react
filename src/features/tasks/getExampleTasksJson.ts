da28a80e3539d22c84a34e129bc6de9dc649f830
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-11 11:56:40 +0100

    Assigned AxiosResponse<TaskData[]> type to the response constant in getExampleTasksJson.

--- src/features/tasks/getExampleTasksJson.ts
+++ src/features/tasks/getExampleTasksJson.ts
@@ -1,11 +1,12 @@
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
+import { TaskData } from "../../common/TaskData";
 
-export const getExampleTasksJson = async () => {    
-    const response =
-        await axios.get("/to-do-list-react/exampleTasks.json");
+export const getExampleTasksJson = async () => {
+    const response: AxiosResponse<TaskData[]> = await axios.get("/to-do-list-react/exampleTasks.json");
 
     if (response.status !== 200) {
         throw new Error(response.statusText);
     }
-    return response.data;
+
+    return (response.data);
 };
