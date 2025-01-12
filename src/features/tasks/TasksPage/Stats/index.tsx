3049bc4ac467c68d694417d6fe42afb3d5d084aa
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-12 09:21:56 +0100

    Converted Stats component file and its styled file to tsx/ts. In Stats component: moved content to return inside empty HTMl elements and JSX.

---
+++ src/features/tasks/TasksPage/Stats/index.tsx
@@ -0,0 +1,37 @@
+import { useSelector } from "react-redux";
+import { Content, ItemWrapper, Item } from "./styled";
+import { Header } from "../../../../common/HeaderContent/styled"
+import { selectTasks, selectIsTasksEmpty, selectTasksLength } from "../../tasksSlice";
+
+const Stats = () => {
+    const tasks = useSelector(selectTasks)
+    const tasksLenght = useSelector(selectTasksLength)
+    const isTasksEmpty = useSelector(selectIsTasksEmpty);
+
+    return (
+        <>
+            {
+                !isTasksEmpty && (
+                    <Content>
+                        <Header $stats>Dane zadań 📊</Header>
+                        <ItemWrapper>
+                            <Item>Liczba wszystkich zadań: <strong>{tasksLenght}</strong></Item>
+                            <Item >
+                                Liczba ukończonych zadań: <strong>{
+                                    ((tasks.filter(({ done }) => done).length / tasksLenght) * 100).toFixed(0)}%
+                                </strong>
+                            </Item>
+                            <Item>
+                                Liczba ważnych zadań: <strong>
+                                    {tasks.filter(({ important, done }) => important && !done).length}
+                                </strong>
+                            </Item>
+                        </ItemWrapper>
+                    </Content>
+                )
+            }
+        </>
+    );
+};
+
+export default Stats;
