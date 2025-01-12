89d3405b8dd330c5899f7b88ddc1a9acc618f75b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-12 11:26:09 +0100

    Moved HeaderPanel component and logic of example tasks fetching to the TasksPage component. In TasksPage: Passed HeaderPanel components in both Section components as title prop.

--- src/features/tasks/TasksPage/Form/index.tsx
+++ src/features/tasks/TasksPage/Form/index.tsx
@@ -3,17 +3,10 @@ import { useDispatch } from "react-redux";
 import { addTask } from "../../tasksSlice";
 import { nanoid } from "@reduxjs/toolkit";
 import { Container, SubmitButton } from "./styled";
-import { HeaderPanel } from "../../../../common/HeaderPanel";
 import { Input } from "../../../../common/Input";
-import { Button } from "../Buttons/styled";
-import { downloadExampleTasks } from "../../tasksSlice";
 import { TaskData } from "../../../../common/TaskData";
 
 const Form = () => {
-    const loadingButtonStatusText = "loading";
-    const initialButtonStatusText = "initial";
-
-    const [buttonStatus, setButtonStatus] = useState(initialButtonStatusText);
     const [newTaskContent, setNewTaskContent] = useState<TaskData["content"]>("Zagrać w Wiedźmina");
 
     const inputRef = useRef<HTMLInputElement>(null);
@@ -45,28 +38,6 @@ const Form = () => {
 
     return (
         <form onSubmit={onFormSubmit}>
-            <HeaderPanel
-                title="Dodaj nowe zadanie"
-                sideContent={
-                    <Button
-                        type="button"
-                        disabled={buttonStatus === loadingButtonStatusText}
-                        onClick={() => {
-                            setButtonStatus(loadingButtonStatusText);
-                            setTimeout(() => {
-                                dispatch(downloadExampleTasks());
-                                setButtonStatus(initialButtonStatusText);
-                            }, 1000)
-                        }}
-                    >
-                        {
-                            buttonStatus === loadingButtonStatusText ?
-                                <>Ładowanie...</> :
-                                <>Pobierz przykładowe zadania</>
-                        }
-                    </Button>
-                }
-            />
             <Container>
                 <Input
                     ref={inputRef}
--- src/features/tasks/TasksPage/index.tsx
+++ src/features/tasks/TasksPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import Section from "../../../common/Section";
 import { HeaderPanel } from "../../../common/HeaderPanel";
@@ -7,22 +7,56 @@ import TasksList from "./TasksList"
 import { Search } from "../Search";
 import Buttons from "./Buttons";
 import { PageTitle } from "../../../common/PageTitle";
+import { Button } from "./Buttons/styled";
+import { useAppDispatch } from "../../../reduxTypedHooks";
+import { downloadExampleTasks } from "../tasksSlice";
 
 export function TasksPage() {
   const titleOfTasksContent = "Lista zadań";
 
+  const loadingButtonStatusText = "loading";
+  const initialButtonStatusText = "initial";
+
+  const [buttonStatus, setButtonStatus] = useState(initialButtonStatusText);
+  const dispatch = useAppDispatch();
   return (
     <>
       <PageTitle content={titleOfTasksContent} />
       <Section
+        title={
+          <HeaderPanel
+            title="Dodaj nowe zadanie"
+            sideContent={
+              <Button
+                type="button"
+                disabled={buttonStatus === loadingButtonStatusText}
+                onClick={() => {
+                  setButtonStatus(loadingButtonStatusText);
+                  setTimeout(() => {
+                    dispatch(downloadExampleTasks());
+                    setButtonStatus(initialButtonStatusText);
+                  }, 1000)
+                }}
+              >
+                {
+                  buttonStatus === loadingButtonStatusText ?
+                    <>Ładowanie...</> :
+                    <>Pobierz przykładowe zadania</>
+                }
+              </Button>
+            }
+          />
+        }
         body={
           <Form />
         }
       />
       <Section
+        title={
+          <HeaderPanel title={titleOfTasksContent} sideContent={<Buttons />} />
+        }
         body={
           <>
-            <HeaderPanel title={titleOfTasksContent} sideContent={<Buttons />} />
             <Stats />
             <Search />
             <TasksList />
