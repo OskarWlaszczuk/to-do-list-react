660245e6c784ede17f06bde5342d46023e863fd1
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-12 09:11:32 +0100

    Converted tasksLocaleStorage functions to ts

--- src/features/tasks/tasksLocaleStorage.js
+++
@@ -1,2 +0,0 @@
-export const saveTasksInLocaleStorage = (tasks, tasksListKey) => localStorage.setItem(tasksListKey, JSON.stringify(tasks));
-export const getInitialTasks = tasksListKey => JSON.parse(localStorage.getItem(tasksListKey)) || [];
