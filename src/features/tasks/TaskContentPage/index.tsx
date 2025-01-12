5f5e103e9facd93ea77372b1b742daab64679d8e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-12 09:19:17 +0100

    Converted TaskContentPage to ts. Changed useParams hook import path. Imported RootState and assigned it to state parameter of callback function passed to the useSelector.

--- src/features/tasks/TaskContentPage/index.tsx
+++ src/features/tasks/TaskContentPage/index.tsx
@@ -1,28 +1,29 @@
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom/cjs/react-router-dom"
+import { useParams } from "react-router-dom"
 import { selectTaskById } from "../tasksSlice";
 import Section from "../../../common/Section";
 import { Title } from "../../../common/Title";
 import { NotFound } from "../../../common/NotFound";
+import { RootState } from "../store";
 
 export const TaskContentPage = () => {
     const { id } = useParams();
-    const task = useSelector((state) => selectTaskById(state, id));
+    const task = useSelector((state: RootState) => selectTaskById(state, id));
 
     return (
         <>
             <Title content="Szczegóły zadania" />
             <Section
                 title={
-                    task ? task.content : <NotFound content="Nie znaleziono szczegółów zadania" />
+                    task ? task.content : <NotFound content="Nie znaleziono zadania" />
                 }
                 body={
-                        <p>
-                            {task ?
-                                <>Ukończone : <strong>{task.done ? "Tak" : "Nie"}</strong></> :
-                                <></>
-                            }
-                        </p>
+                    <p>
+                        {task ?
+                            <>Ukończone : <strong>{task.done ? "Tak" : "Nie"}</strong></> :
+                            <></>
+                        }
+                    </p>
                 }
             />
         </>
