e36058ba444023ebe5a01ac1be27dc2fbbffd45a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-12 11:14:49 +0100

    Passed colors from theme to properties in Wrapper.

--- src/common/Section/styled.ts
+++ src/common/Section/styled.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-    background-color: white;
-    border: 2px solid rgba(179, 174, 174, 0.742);
+    background-color: ${({ theme }) => theme.colors.white};
+    border: 2px solid ${({ theme }) => theme.colors.nobel};
     border-radius: 3px;
-    box-shadow: 0px 0px 11px -3px #525251be;
+    box-shadow: 0px 0px 11px -3px ${({ theme }) => theme.colors.lightBlack};
     width: 60%;
     margin-top: 15px;
     padding: 20px;
