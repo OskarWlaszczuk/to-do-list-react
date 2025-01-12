9a5e10c6f318bca2c9288a416dd2821dc26ce209
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-12 09:11:57 +0100

    Converted tasksSaga and its generator functions to ts

---
+++ src/features/tasks/tasksSaga.ts
@@ -0,0 +1,42 @@
+import { call, CallEffect, put, PutEffect, select, SelectEffect, takeEvery, takeLatest, Effect } from "redux-saga/effects";
+import { downloadExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { getExampleTasksJson } from "./getExampleTasksJson";
+import { saveTasksInLocaleStorage } from "./tasksLocaleStorage";
+import { tasksListKey } from "../../tasksListKey";
+import { TaskData } from "../../common/TaskData";
+
+function* downloadExampleTasksHandler(): Generator<
+    PutEffect | CallEffect<TaskData[]>,
+    void,
+    TaskData[]
+> {
+    try {
+        const exampleTasks = yield call(getExampleTasksJson);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        console.error(error)
+    };
+};
+
+function* saveTasksInLocaleStorageHandler(): Generator<
+    SelectEffect | CallEffect<void>,
+    void,
+    TaskData[]
+> {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocaleStorage, tasks, tasksListKey);
+};
+
+type DownloadExampleTasksAction = ReturnType<typeof downloadExampleTasks>;
+
+export function* tasksSaga(): Generator<
+    Effect,
+    void,
+    DownloadExampleTasksAction
+> {
+    yield takeLatest<DownloadExampleTasksAction>(
+        downloadExampleTasks.type,
+        downloadExampleTasksHandler
+    );
+    yield takeEvery("*", saveTasksInLocaleStorageHandler)
+};
