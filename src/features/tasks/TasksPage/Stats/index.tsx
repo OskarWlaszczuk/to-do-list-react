eccf8aed15fa4a9fdc03c8c5fdabb6c1c376f3c6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-13 07:26:12 +0100

    In Stats: Created statsRenderData object array. Each object constains statistic data, like description and result. Render stats by using map method on statsRenderData. Imported new selector called selectImportantTasksLength and assigned its value to importantTasksLength constant.

--- src/features/tasks/TasksPage/Stats/index.tsx
+++ src/features/tasks/TasksPage/Stats/index.tsx
@@ -1,14 +1,28 @@
 import { Content, ItemWrapper, Item } from "./styled";
-import { selectTasks, selectIsTasksListEmpty, selectTasksLength } from "../../tasksSlice";
+import { selectTasks, selectIsTasksListEmpty, selectTasksLength, selectImportantTasksLength } from "../../tasksSlice";
 import { SectionHeader } from "../../../../common/SectionHeader";
 import { useAppSelector } from "../../../../reduxTypedHooks";
 
 const Stats = () => {
     const tasks = useAppSelector(selectTasks)
     const tasksLenght = useAppSelector(selectTasksLength)
+    const importantTasksLength = useAppSelector(selectImportantTasksLength);
     const isTasksEmpty = useAppSelector(selectIsTasksListEmpty);
 
-   
+    const statsRenderData = [
+        {
+            description: "Liczba wszystkich zadań",
+            result: tasksLenght,
+        },
+        {
+            description: "Liczba ważnych zadań",
+            result:importantTasksLength,
+        },
+        {
+            description: "Procent ukończonych zadań",
+            result: <>{((tasks.filter(({ done }) => done).length / tasksLenght) * 100).toFixed(0)}%</>,
+        },
+    ]
 
     return (
         <>
@@ -17,17 +31,11 @@ const Stats = () => {
                     <Content>
                         <SectionHeader $spaceAround>Dane zadań 📊</SectionHeader>
                         <ItemWrapper>
-                            <Item>Liczba wszystkich zadań: <b>{tasksLenght}</b></Item>
-                            <Item >
-                                Liczba ukończonych zadań: <b>{
-                                    ((tasks.filter(({ done }) => done).length / tasksLenght) * 100).toFixed(0)}%
-                                </b>
-                            </Item>
-                            <Item>
-                                Liczba ważnych zadań: <b>
-                                    {tasks.filter(({ important, done }) => important && !done).length}
-                                </b>
-                            </Item>
+                            {
+                                statsRenderData.map(({ description, result }) => (
+                                    <Item >{description}: <b>{result}</b></Item>
+                                ))
+                            }
                         </ItemWrapper>
                     </Content>
                 )
