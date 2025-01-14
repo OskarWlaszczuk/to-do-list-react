446a574aed61ebef754d62797fa640df581a742a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-14 19:56:08 +0100

    Moved renderSearchingTasks to useRenderSearchingTasks hook.

--- src/features/tasks/TasksPage/TasksList/index.tsx
+++ src/features/tasks/TasksPage/TasksList/index.tsx
@@ -1,22 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import {
-  selectTaskByQuery,
   selectHideDoneTasks,
-  removeTask,
-  toggleImportantContent,
-  toggleTaskDone,
   selectTasksLength,
   selectAreAllTasksDone,
 } from "../../tasksSlice";
 import {
   List,
-  TaskItem,
-  RemoveTaskButton,
-  ToggleDoneButton,
-  TaskContent,
-  TopButtonsPanel,
-  TopButtonsPanelItem,
-  TaskDetailsLink
 } from "./styled";
 import { NotFoundMessage } from "../../../../common/NotFoundMessage";
 import { Message } from "./EmptyTasksListMessage/Message";
@@ -24,92 +13,18 @@ import { selectIsSearchTasksEmpty } from "../../tasksSlice";
 import { queryKey } from "../../queryKey";
 import { useQueryParameter } from "../../useQueryParameter";
 import { RootState } from "../../store";
-import { useAppDispatch, useAppSelector } from "../../../../reduxTypedHooks";
+import { useAppSelector } from "../../../../reduxTypedHooks";
+import { useRenderSearchingTasks } from "../hooks/useRenderSearchingTasks";
 
 const TasksList = () => {
   const queryValue = useQueryParameter(queryKey);
+  const renderSearchingTasks = useRenderSearchingTasks();
 
-  const searchTasks = useSelector((state: RootState) => selectTaskByQuery(state, queryValue));
+  const areAllTasksDone = useAppSelector(selectAreAllTasksDone);
   const isSearchTasksEmpty = useSelector((state: RootState) => selectIsSearchTasksEmpty(state, queryValue));
+
   const hideDoneTasks = useSelector(selectHideDoneTasks);
   const tasksLength = useAppSelector(selectTasksLength);
-  const areAllTasksDone = useAppSelector(selectAreAllTasksDone);
-
-  const dispatch = useAppDispatch();
-
-  const renderSearchingTasks = () => {
-    return (
-      searchTasks?.map(({ id, done, content, important }) => {
-
-        const handleToggleDone = () => dispatch(toggleTaskDone(id));
-        const handleTaskRemove = () => dispatch(removeTask(id));
-
-        const capitalizedContent = content.slice(0, 1).toUpperCase() + content.slice(1);
-
-        const renderTopButtonsPanel = () => {
-
-          const handleToggleImportant = () => dispatch(toggleImportantContent(id));
-          type TopPanelButtonsActions = ReturnType<typeof handleToggleImportant>
-
-          interface TopPanelButton {
-            title: string;
-            disabledCondition: boolean;
-            activatedCondition: boolean;
-            actionHandler: () => TopPanelButtonsActions;
-            content: string;
-          }
-
-          const topButtonsPanel: TopPanelButton[] = [
-            {
-              title: "Ustaw, jako ważne",
-              disabledCondition: done,
-              activatedCondition: important && !done,
-              actionHandler: handleToggleImportant,
-              content: "B",
-            },
-          ];
-
-          return (
-            <TopButtonsPanel>
-              {
-                topButtonsPanel.map(({ title, disabledCondition, activatedCondition, actionHandler, content }, index) => (
-                  <TopButtonsPanelItem
-                    key={index}
-                    title={title}
-                    disabled={disabledCondition}
-                    $activated={activatedCondition}
-                    onClick={actionHandler}
-                  >
-                    {content}
-                  </TopButtonsPanelItem>
-                ))
-              }
-            </TopButtonsPanel>
-          );
-        };
-
-        return (!hideDoneTasks || (hideDoneTasks && !done)) && (
-          <TaskItem key={id}>
-            {renderTopButtonsPanel()}
-            <ToggleDoneButton onClick={handleToggleDone}>{done ? "✔" : ""}</ToggleDoneButton>
-            <TaskDetailsLink
-              title="Wejdź w szczegóły zadania"
-              to={`/tasks/${id}`}
-            >
-              <TaskContent
-                $justified={content.length > 160 ? true : false}
-                $important={important && !done}
-                $donedItem={done}
-              >
-                {capitalizedContent}
-              </TaskContent>
-            </TaskDetailsLink>
-            <RemoveTaskButton onClick={handleTaskRemove}>🗑️</RemoveTaskButton>
-          </TaskItem>
-        )
-      })
-    );
-  };
 
   const renderListContent = () => {
     if (isSearchTasksEmpty && !!queryValue) return <NotFoundMessage content="Nie znaleziono zadania" />
