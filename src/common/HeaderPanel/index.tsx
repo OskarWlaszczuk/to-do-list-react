e862ef6cfc2d3af0fe6722ba511404e97b861241
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-14 07:22:38 +0100

    Created HeaderPanel2 that is alternative version of HeaderPanel. HeaderPanel2 gets in prop buttonsRenderData. It's an array where each element is object that contains button data required to render it as Button component. Inside HeaderPanel2 created renderButtons function that iterates on buttonsRenderData elements using map method and returns Button elements.

--- src/common/HeaderPanel/index.tsx
+++ src/common/HeaderPanel/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { Container } from "./styled";
 import { SectionHeader } from "../SectionHeader";
+import { Button, ButtonsContainer } from "../../features/tasks/TasksPage/Buttons/styled";
 
 interface HeaderPanelProps {
     title: string;
@@ -13,3 +14,39 @@ export const HeaderPanel = ({ title, sideContent }: HeaderPanelProps) => (
         {sideContent}
     </Container>
 );
+
+interface ButtonRenderData {
+    clickEventHandler: () => void;
+    disabledCondition: boolean;
+    content: ReactNode;
+}
+
+interface HeaderPanelProps2 {
+    title: string;
+    buttonsRenderData: ButtonRenderData[];
+}
+
+export const HeaderPanel2 = ({ title, buttonsRenderData }: HeaderPanelProps2) => {
+
+    const renderButtons = () => (
+        buttonsRenderData.map(({ clickEventHandler, disabledCondition, content }, index) => (
+            <Button
+                type="button"
+                key={index}
+                onClick={clickEventHandler}
+                disabled={disabledCondition}
+            >
+                {content}
+            </Button>
+        ))
+    );
+
+    return (
+        <Container>
+            <SectionHeader>{title}</SectionHeader>
+            <ButtonsContainer>
+                {renderButtons()}
+            </ButtonsContainer>
+        </Container>
+    );
+};
