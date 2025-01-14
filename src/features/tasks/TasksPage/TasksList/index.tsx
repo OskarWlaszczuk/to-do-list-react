a6289d524cf89362401cef938a38435347f30d97
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-14 18:28:24 +0100

    In TasksList: Moved tasks list iteration and rendering to renderSearchingTasks function. List's content selecting condition moved to renderListContent function, replaced ternary operator with if/else. Call renderListContent inside JSX in component return content. Moved onClick event handlers to function declarations inside map iteration in renderSearchingTasks function. Imported selectTasksLength selector and assigned its value to tasksLength constant.

--- src/features/tasks/TasksPage/TasksList/index.tsx
+++ src/features/tasks/TasksPage/TasksList/index.tsx
@@ -1,71 +1,94 @@
 import { useSelector, useDispatch } from "react-redux";
-import { selectTaskByQuery, selectHideDoneTasks, selectTasks } from "../../tasksSlice";
-import { toggleTaskDone, removeTask, toggleImportantContent } from "../../tasksSlice";
-import { List, Item, Button, ToggleDoneButton, Content, ButtonsBar, ButtonsBarItem, StyledLink } from "./styled";
-import { NotFound } from "../../../../common/NotFound";
-import { EmptyTasksList } from "./EmptyTasksList";
+import {
+  selectTaskByQuery,
+  selectHideDoneTasks,
+  removeTask,
+  toggleImportantContent,
+  selectTasks,
+  toggleTaskDone,
+  selectTasksLength,
+} from "../../tasksSlice";
+import {
+  List,
+  Item,
+  Button,
+  ToggleDoneButton,
+  Content,
+  ButtonsBar,
+  ButtonsBarItem,
+  StyledLink
+} from "./styled";
+import { NotFoundMessage } from "../../../../common/NotFoundMessage";
+import { EmptyTasksListMessage } from "./EmptyTasksListMessage";
 import { selectIsSearchTasksEmpty } from "../../tasksSlice";
 import { queryKey } from "../../queryKey";
 import { useQueryParameter } from "../../useQueryParameter";
 import { RootState } from "../../store";
+import { useAppSelector } from "../../../../reduxTypedHooks";
 
 const TasksList = () => {
   const queryValue = useQueryParameter(queryKey);
 
   const searchTasks = useSelector((state: RootState) => selectTaskByQuery(state, queryValue));
   const isSearchTasksEmpty = useSelector((state: RootState) => selectIsSearchTasksEmpty(state, queryValue));
-
-  const allTasks = useSelector(selectTasks);
   const hideDoneTasks = useSelector(selectHideDoneTasks);
-
+  const tasksLength = useAppSelector(selectTasksLength);
+  console.log(hideDoneTasks);
   const dispatch = useDispatch();
 
+  const renderSearchingTasks = () => {
+    return (
+      searchTasks?.map(({ id, done, content, important }) => {
+
+        const handleToggleImportant = () => dispatch(toggleImportantContent(id));
+        const handleToggleDone = () => dispatch(toggleTaskDone(id));
+        const handleTaskRemove = () => dispatch(removeTask(id));
+
+        const capitalizedContent = content.slice(0, 1).toUpperCase() + content.slice(1);
+
+        return (!hideDoneTasks || (hideDoneTasks && !done)) && (
+          <Item key={id}>
+            <ButtonsBar>
+              <ButtonsBarItem
+                title="Ustaw, jako ważne"
+                disabled={done}
+                $activated={important && !done}
+                onClick={handleToggleImportant}
+              >
+                B
+              </ButtonsBarItem>
+            </ButtonsBar>
+            <ToggleDoneButton onClick={handleToggleDone}>
+              {done ? "✔" : ""}
+            </ToggleDoneButton>
+            <StyledLink
+              title="Wejdź w szczegóły zadania"
+              to={`/tasks/${id}`}
+            >
+              <Content
+                $justified={content.length > 160 ? true : false}
+                $important={important && !done}
+                $donedItem={done}
+              >
+                {capitalizedContent}
+              </Content>
+            </StyledLink>
+            <Button onClick={handleTaskRemove}>🗑️</Button>
+          </Item>
+        )
+
+      })
+    );
+  };
+
+  const renderListContent = () => {
+    if (isSearchTasksEmpty && !!queryValue) return <NotFoundMessage content="Nie znaleziono zadania" />
+    if (!tasksLength) return <EmptyTasksListMessage />
+    else return <List>{renderSearchingTasks()}</List>
+  };
+
   return (
-    <>
-      {
-        isSearchTasksEmpty && !!queryValue ?
-          <NotFound content="Nie znaleziono zadania" /> :
-          (!allTasks.length ?
-            <EmptyTasksList /> :
-            <List>
-              {searchTasks.map(({ id, done, content, important }) => {
-                return (
-                  (!hideDoneTasks || (hideDoneTasks && !done)) && (
-                    <Item key={id}>
-                      <ButtonsBar>
-                        <ButtonsBarItem
-                          title="Ustaw, jako ważne"
-                          disabled={done}
-                          $activated={important && !done}
-                          onClick={() => dispatch(toggleImportantContent(id))}
-                        >
-                          B
-                        </ButtonsBarItem>
-                      </ButtonsBar>
-                      <ToggleDoneButton onClick={() => dispatch(toggleTaskDone(id))}>
-                        {done ? "✔" : ""}
-                      </ToggleDoneButton>
-                      <StyledLink
-                        title="Wejdź w szczegóły zadania"
-                        to={`/tasks/${id}`}
-                      >
-                        <Content
-                          $justified={content.length > 160 ? true : false}
-                          $important={important && !done}
-                          $donedItem={done}
-                        >
-                          {content.slice(0, 1).toUpperCase() + content.slice(1)}
-                        </Content>
-                      </StyledLink>
-                      <Button onClick={() => dispatch(removeTask(id))}>🗑️</Button>
-                    </Item>
-                  )
-                );
-              })}
-            </List>
-          )
-      }
-    </>
+    <>{renderListContent()}</>
   );
 };
 
