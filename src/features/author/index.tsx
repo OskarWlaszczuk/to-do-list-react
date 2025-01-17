5f41a6264f49a2a3bf0165ea478bed9acc6acd35
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-01-17 06:18:25 +0100

    Formatted code in AuthorPage.

--- src/features/author/index.tsx
+++ src/features/author/index.tsx
@@ -10,21 +10,19 @@ export const AuthorPage = () => (
         <Section
             title={<SectionHeader $centered>Oskar Właszczuk</SectionHeader>}
             body={
-
                 <>
                     <Image src="https://avatars.githubusercontent.com/u/155220171?v=4" />
                     <CenteredParagraph>
                         Cześć! <br />Jestem młodym, zdeterminowanym i zdescyplinowanym chłopakiem, który z pasją podchodzi do siłowni i programowania. Każdego dnia staram się rozwijać swoje umiejętności, zarówno fizyczne, jak i umysłowe, starając się przy okazji dobrze, przy tym bawić. Moje zaangażowanie w treningi i kodowanie pozwala mi nieustannie pokonywać własne granice i osiągać nowe cele.
                     </CenteredParagraph>
                     <CenteredParagraph>
-                        Jeżeli chcesz się dowiedzieć więcej o projekcie, sprawdź moje
+                        Jeżeli chcesz się dowiedzieć więcej o projekcie, sprawdź moje{" "}
                         <a
                             title="Repozytorium Listy zadań na GitHub"
                             target="_blank"
                             rel="noreferrer"
                             href="https://github.com/OskarWlaszczuk/to-do-list-react.git"
-                        >
-                            repozytorium na GitHub
+                        >repozytorium na GitHub
                         </a>
                     </CenteredParagraph>
                 </>
